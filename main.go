33c9b5296b80c485e0d5ce466cfad0ce402eed67
Leor <alonl9001@gmail.com>
2021-03-15 04:15:44 +0000

    Main now calls router service and initializes the admin user in the DB if the user doesn't already exist

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"iotdashboard/router"
+	"log"
 )
 
 func main() {
 	router,err := router.NewRouter(":8080",":9090")
 	if err != nil{
 		panic(err)
+	}
+
+	err = router.Ctrlr.PSQL.AddNewUser("e@g.c", "test")
+	if err != nil{
+		log.Printf("Admin user already exists in DB")
 	}
 
 
